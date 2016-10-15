d8733c03aeebdb02f264a625f3b59b09e8f0abdb
Peter Kolski <peter@bildpeter.de>
2016-10-15 18:37:48 +0200

    CleanUp

--- punksoup.py
+++ punksoup.py
@@ -5,38 +5,26 @@ import requests
 from bs4 import BeautifulSoup
 
 print('geileWelt')
-url = "http://jedermenschistzuviel.stabo.org/texte.php"
 
-# zum "weiterblaettern":
-
-url_page_2 = url + '&page=' + str(2) + '&s=relevance'
-
-# def get_data_from_url(url, 10): ## wie viele Seiten sollen durchsucht werden?
-
-# r = requests.get("url")
-# myRequests = requests.get("http://jedermenschistzuviel.stabo.org/texte.php")
 myRequests = requests.get("http://jedermenschistzuviel.stabo.org/texte.php?show=2&what=7357")
 myRequests.content
 
 soup = BeautifulSoup( myRequests.content )
-# print( soup.prettify() )
+websiteData = soup.html.find_all("td")
+textData 	= websiteData[6].find_all_next(text=True)
 
-g_data = soup.html.find_all( "td" )
-# g_data = soup.findAll("td", attrs={'div': None})
-# print( g_data[6] )
-
-newData2 = g_data[6].find_all_next( text=True)
-# print( newData2 )
+i = 0
+while textData[i] != "zurück zur Bandübersicht":
+	print(textData[i])
+	i = i+1
 
 # --- TITLE
 # newData = g_data[6].find_all("h1", text=True)
 # print( newData )
 
 
-i = 0
-while newData2[i] != "zurück zur Bandübersicht":
-	print(newData2[i])
-	i = i+1
+
+
 
 
 
