d029c30daa0124befe96af9b7b25ef8023a13c59
larimadora <scherchchristiane@googlemail.com>
2016-10-15 19:14:15 +0200

    file added

--- punksoup.py
+++ punksoup.py
@@ -9,7 +9,7 @@ print('geileWelt')
 myRequests = requests.get("http://jedermenschistzuviel.stabo.org/texte.php?show=2&what=7357")
 myRequests.content
 
-soup = BeautifulSoup( myRequests.content )
+soup = BeautifulSoup( myRequests.content.decode('ISO-8859-1', 'ignore') )
 websiteData = soup.html.find_all("td")
 textData 	= websiteData[6].find_all_next(text=True)
 
@@ -17,6 +17,8 @@ i = 0
 while textData[i] != "zurück zur Bandübersicht":
 	print(textData[i])
 	i = i+1
+
+
 
 # --- TITLE
 # newData = g_data[6].find_all("h1", text=True)
