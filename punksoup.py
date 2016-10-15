fc5add1b64ae062412dcf74fb45ffa7ff32ed624
Peter Kolski <peter@bildpeter.de>
2016-10-15 18:34:06 +0200

    Text is read with title

--- punksoup.py
+++ punksoup.py
@@ -7,39 +7,63 @@ from bs4 import BeautifulSoup
 print('geileWelt')
 url = "http://jedermenschistzuviel.stabo.org/texte.php"
 
-## zum "weiterblättern":
+# zum "weiterblaettern":
 
 url_page_2 = url + '&page=' + str(2) + '&s=relevance'
 
 # def get_data_from_url(url, 10): ## wie viele Seiten sollen durchsucht werden?
 
 # r = requests.get("url")
-r = requests.get("http://jedermenschistzuviel.stabo.org/texte.php")
-r.content
+# myRequests = requests.get("http://jedermenschistzuviel.stabo.org/texte.php")
+myRequests = requests.get("http://jedermenschistzuviel.stabo.org/texte.php?show=2&what=7357")
+myRequests.content
 
-soup = BeautifulSoup(r.content)
-print(soup.prettify())
+soup = BeautifulSoup( myRequests.content )
+# print( soup.prettify() )
+
+g_data = soup.html.find_all( "td" )
+# g_data = soup.findAll("td", attrs={'div': None})
+# print( g_data[6] )
+
+newData2 = g_data[6].find_all_next( text=True)
+# print( newData2 )
+
+# --- TITLE
+# newData = g_data[6].find_all("h1", text=True)
+# print( newData )
+
+
+i = 0
+while newData2[i] != "zurück zur Bandübersicht":
+	print(newData2[i])
+	i = i+1
+
+
+
+# i = 0
+# for item in g_data:
+# 	print( item.text )
 
 ## alle links zwischen </a> tags finden:
 ## link-texte und absolute links finden:
 
-links = soup.find_all("a")
-for link in links:
-	#if "http" in link.get("href"): ## gibt die absoluten links
-		print("<a href='%s'>%s</a>" %(link.get("href"), link.text))
+# myLinks = soup.find_all("a")
+# for link in myLinks:
+# 	if "http" in link.get("href"): ## gibt die absoluten links
+		# print("<a href='%s'>%s</a>" %(link.get("href"), link.text))
 
 
 
-## weitere informationen crawlen:
+# weitere informationen crawlen:
 
-g_data = soup.find_all("div") ## ("", {"class": "info"}) für weitere Infos
-print(g_data)
+# g_data = soup.find_all("div") ## ("", {"class": "info"}) fuer weitere Infos
+# print(g_data)
 
-for item in g_data:
-	print(item.text)
-	print(item)
-	print(item.contents)
-	print(item.contents[0])
+# for item in g_data:
+# 	print(item.text)
+# 	print(item)
+# 	print(item.contents)
+# 	print(item.contents[0])
 	#print(item.contents[0].text) ## printet unterclassen [0] von div mit aus
 	#print(item.contents[0].find_all("a", {"class": "id"})[0].text)
 	
