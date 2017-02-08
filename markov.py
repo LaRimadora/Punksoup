12e2966138cecb49fb35570e94f7829586fa7b60
Peter Kolski <peter@bildpeter.de>
2017-02-08 08:32:42 +0100

    Trying NLTK integration

--- markov.py
+++ markov.py
@@ -11,3 +11,22 @@ for i in range(5):
 
 for i in range(3):
     print(text_model.make_short_sentence(140) + '\n')
+
+
+# import markovify
+import nltk
+import re
+
+class POSifiedText( markov.Text ):
+    def word_split(self, sentence):
+        words = re.split(self.word_split_pattern, sentence)
+        words = [ "::".join(tag) for tag in nltk.pos_tag(words) ]
+        return words
+
+    def word_join(self, words):
+        sentence = " ".join(word.split("::")[0] for word in words)
+        return sentence
+
+posModel = POSifiedText.Text(text)
+for i in range(5):
+    print( posModel.make_short_sentence(100) + '\n' )
